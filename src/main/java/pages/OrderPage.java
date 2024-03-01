8daf8ca135c5d01f5192ddd8b55fe7e535ca3971
iuditabigiu <iudita_zan@yahoo.com>
2024-03-01 19:59:37 +0000

    New Ones

--- src/main/java/pages/OrderPage.java
+++ src/main/java/pages/OrderPage.java
@@ -5,51 +5,109 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class OrderPage extends BasePage{
+public class OrderPage extends BasePage {
     public OrderPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
     }
-    @FindBy ( css = "#root > div > div:nth-child(2) > div:nth-child(2) > div.row.row-cols-xl-4.row-cols-lg-3.row-cols-md-2.row-cols-sm-2.row-cols-1 > div:nth-child(1) > div > div.text-muted.text-center.card-footer > button:nth-child(2) > svg > path")
+
+    @FindBy(css = "#root > div > div:nth-child(2) > div:nth-child(2) > div.row.row-cols-xl-4.row-cols-lg-3.row-cols-md-2.row-cols-sm-2.row-cols-1 > div:nth-child(1) > div > div.text-muted.text-center.card-footer > button:nth-child(2) > svg > path")
     private WebElement AwesomeGraniteChipsCart;
-    public void AddAwesomeGraniteChipsToCart(){
+
+    public void AddAwesomeGraniteChipsToCart() {
         AwesomeGraniteChipsCart.click();
     }
-    @FindBy ( css = ".shopping_cart_badge")
+
+    @FindBy(css = ".shopping_cart_badge")
     private WebElement ShoppingCart;
-    public void ClickOnShoppingCart(){
+
+    public void ClickOnShoppingCart() {
         ShoppingCart.click();
     }
-    @FindBy ( css = ".btn-success")
+
+    @FindBy(css = ".btn-success")
     private WebElement CheckoutButton;
-    public void ClickOnCheckoutButton (){
+
+    public void ClickOnCheckoutButton() {
         CheckoutButton.click();
     }
-    @FindBy ( id = "first-name")
+
+    @FindBy(id = "first-name")
     private WebElement AddFirstNameInFirstNameField;
-    public void InsertNameInFirstNameField (String text ){
+
+    public void InsertNameInFirstNameField(String text) {
         AddFirstNameInFirstNameField.sendKeys(text);
     }
-    @FindBy ( id = "last-name")
+
+    @FindBy(id = "last-name")
     private WebElement AddLastNameInLastNameField;
-    public void InsertLastNameInLastNameField ( String text){
+
+    public void InsertLastNameInLastNameField(String text) {
         AddLastNameInLastNameField.sendKeys(text);
     }
-    @FindBy  ( xpath= "//*[@id=\"address\"]")
+
+    @FindBy(xpath = "//*[@id=\"address\"]")
     private WebElement AddAddressInAddressField;
-    public void InsertAddressInAddressField (String text){
+
+    public void InsertAddressInAddressField(String text) {
         AddAddressInAddressField.sendKeys(text);
     }
-    @FindBy ( css = ".btn-success")
+
+    @FindBy(css = ".btn-success")
     private WebElement ContinueCheckoutButton;
-    public void ClickOnContinueCheckoutButton (){
+
+    public void ClickOnContinueCheckoutButton() {
+
         ContinueCheckoutButton.click();
     }
-    @FindBy ( css = "a[href='#/checkout-complete']")
+
+    @FindBy(css = "a[href='#/checkout-complete']")
     private WebElement CompleteYourOrderButton;
-    public void ClickOnCompleteYourOrderButton (){
+
+    public void ClickOnCompleteYourOrderButton() {
         CompleteYourOrderButton.click();
     }
-    @FindBy (css = ".text-center")
+
+    @FindBy(css = ".text-center")
     private WebElement MessageAfterCompleteTheOrder;
+    //verifica de aici
+    @FindBy(css = "#root > div > div:nth-child(2) > div:nth-child(2) > div.row.row-cols-xl-4.row-cols-lg-3.row-cols-md-2.row-cols-sm-2.row-cols-1 > div:nth-child(2) > div > div.text-muted.text-center.card-footer > button:nth-child(2) > svg > path")
+    private WebElement AwesomeMetalChairChart;
+
+    public void AddAwesomeMetalChairInChart() {
+        AwesomeMetalChairChart.click();
+    }
+
+    @FindBy(css = "#root > div > div:nth-child(2) > div:nth-child(2) > div.row.row-cols-xl-4.row-cols-lg-3.row-cols-md-2.row-cols-sm-2.row-cols-1 > div:nth-child(3) > div > div.text-muted.text-center.card-footer > button:nth-child(2) > svg > path")
+    private WebElement AwesomeSoftShirtChart;
+
+    public void AddAwesomeSoftShirtInChart() {
+        AwesomeSoftShirtChart.click();
+    }
+
+    @FindBy(css = "#root > div > div:nth-child(2) > div:nth-child(2) > div.row.row-cols-xl-1.row-cols-lg-1.row-cols-md-1.row-cols-sm-1.row-cols-1 > div > div > div > div:nth-child(1) > div > button:nth-child(2) > svg")
+    private WebElement PlusSignButton;
+
+    public void ClickOnPlusSignButtonToAddAnotherAwesomeGraniteChipsProduct() {
+        PlusSignButton.click();
+    }
+
+    @FindBy(css = "#root > div > div:nth-child(2) > div:nth-child(2) > div.row.row-cols-xl-4.row-cols-lg-3.row-cols-md-2.row-cols-sm-2.row-cols-1 > div:nth-child(10) > div > div.text-muted.text-center.card-footer > button:nth-child(2) > svg > path")
+    private WebElement RefinedFrozenMouse;
+
+    public void AddRefinedFrozenMouseToCart() {
+        RefinedFrozenMouse.click();
+    }
+
+    @FindBy(css = "#root > div > div:nth-child(2) > div:nth-child(2) > div.row.row-cols-xl-4.row-cols-lg-3.row-cols-md-2.row-cols-sm-2.row-cols-1 > div:nth-child(9) > div > div.text-muted.text-center.card-footer > button:nth-child(2) > svg > path")
+    private WebElement PracticalWoodenBaconCart;
+    public void AddPracticalWoodenBaconToCart(){
+        PracticalWoodenBaconCart.click();
+    }
+    @FindBy ( css = "#root > div > div:nth-child(2) > div:nth-child(2) > div.row.row-cols-xl-1.row-cols-lg-1.row-cols-md-1.row-cols-sm-1.row-cols-1 > div > div:nth-child(1) > div > div:nth-child(5) > button > svg > path")
+    private WebElement BinButtonForRefinedFrozenMouse;
+    public void ClickOnBinButtonToRemoveRefinedFrozenMouseFromCart (){
+        BinButtonForRefinedFrozenMouse.click();
+    }
 }
+
--- src/test/java/OrderTests.java
+++ src/test/java/OrderTests.java
@@ -27,4 +27,39 @@ public class OrderTests extends BasePage {
         System.out.println(actualResult);
         Assert.assertTrue(actualResult.contains("Thank you for your order!"));
     }
-}
+   @Test
+    public void AddProductsToChartPressPlusSignAndCompleteTheOrder (){
+        OrderPage.AddAwesomeGraniteChipsToCart();
+        OrderPage.AddAwesomeSoftShirtInChart();
+        OrderPage.AddAwesomeMetalChairInChart();
+        OrderPage.ClickOnShoppingCart();
+        OrderPage.ClickOnPlusSignButtonToAddAnotherAwesomeGraniteChipsProduct();
+        OrderPage.ClickOnCheckoutButton();
+        OrderPage.InsertNameInFirstNameField("iuditha");
+        OrderPage.InsertLastNameInLastNameField("Bigiu");
+        OrderPage.InsertAddressInAddressField("Bulgarus 108");
+        OrderPage.ClickOnContinueCheckoutButton();
+        OrderPage.ClickOnCompleteYourOrderButton();
+        String actualResult = driver.findElement(By.cssSelector(".text-center")).getAttribute("innerText");
+        System.out.println(actualResult);
+        Assert.assertTrue(actualResult.contains("Thank you for your order!"));
+    }
+    @Test
+    public void AddProductsToCartAndRemoveProductFromCartAndCompleteTheOrder (){
+        OrderPage.AddPracticalWoodenBaconToCart();
+        OrderPage.AddRefinedFrozenMouseToCart();
+        OrderPage.ClickOnShoppingCart();
+        OrderPage.ClickOnBinButtonToRemoveRefinedFrozenMouseFromCart();
+        OrderPage.ClickOnCheckoutButton();
+        OrderPage.InsertNameInFirstNameField("iuditha");
+        OrderPage.InsertLastNameInLastNameField("Bigiu");
+        OrderPage.InsertAddressInAddressField("Bulgarus 108");
+        OrderPage.ClickOnContinueCheckoutButton();
+        OrderPage.ClickOnCompleteYourOrderButton();
+        String actualResult = driver.findElement(By.cssSelector(".text-center")).getAttribute("innerText");
+        System.out.println(actualResult);
+        Assert.assertTrue(actualResult.contains("Thank you for your order!"));
+    }
+
+    }
+
