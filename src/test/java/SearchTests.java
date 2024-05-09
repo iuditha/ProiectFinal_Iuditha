f94715de571282c526bbbb9e97350b51af6adeda
iuditabigiu <iudita_zan@yahoo.com>
2024-05-09 18:47:37 +0100

    New One

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -37,6 +37,16 @@ public class SearchTests extends BasePage {
         System.out.println(actualResult);
         Assert.assertTrue(actualResult.contains("Gorgeous Soft Pizza"));
     }
-
+    @Test
+    public void InsertBaconWordInSearchFieldAndClickOnSearchButton (){
+        SearchPage.InsertTextInSearchField("Bacon");
+        SearchPage.ClickOnSearchButton();
+        String actualResult = driver.findElement(By.cssSelector("a[href='#/product/4']")).getAttribute("innerText");
+        System.out.println(actualResult);
+        Assert.assertTrue(actualResult.contains("Practical Wooden Bacon"));
+        String actualResult2 = driver.findElement(By.cssSelector("a[href='#/product/6']")).getAttribute("innerText");
+        System.out.println(actualResult2);
+        Assert.assertTrue(actualResult2.contains("Practical Wooden Bacon"));
+    }
 
 }
